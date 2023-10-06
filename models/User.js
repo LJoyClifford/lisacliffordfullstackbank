beadc3cb2dee9e05bcb27c079f613f2c23787088
Lisa Clifford <78510843+LJoyClifford@users.noreply.github.com>
2023-10-06 09:46:18 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,13 +5,13 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
+   
 
   },
   email: {
     type: String,
     required: true,
-    unique: true
+  
   },
   password: {
     type: String,
